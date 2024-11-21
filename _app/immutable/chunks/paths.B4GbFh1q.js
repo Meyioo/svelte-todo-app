521933ac4a579162b6856a596135ceb791023228
Meyioo <tim.meyerhoff@outlook.de>
2024-11-21 16:26:56 +0100

    Updates

--- _app/immutable/chunks/paths.B4GbFh1q.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1npfr0d)==null?void 0:s.base)??"/svelte-todo-app";var e;const a=((e=globalThis.__sveltekit_1npfr0d)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/entry/start.CXGIiqW9.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Bixnxpc3.js";export{t as start};
