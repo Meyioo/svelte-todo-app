6df60785ae5f5cabf51a8b572b444fb8a425c54f
Meyioo <54744929+Meyioo@users.noreply.github.com>
2024-12-06 08:52:08 +0000

    Deploying to gh-pages from @ Meyioo/svelte-todo-app@3c3cacd1f39e08bbf7e4fe969f6e7ed692cc3ea1 🚀

---
+++ _app/immutable/chunks/stores.EII2p74n.js
@@ -0,0 +1,1 @@
+import{s as e}from"./entry.DOq7BaDv.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
---
+++ _app/immutable/entry/start.iXWecGpL.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DOq7BaDv.js";export{t as start};
