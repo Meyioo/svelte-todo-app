09dddac0ebc7c87cca0d0e639dafc805bab9bae7
Meyioo <54744929+Meyioo@users.noreply.github.com>
2024-12-10 17:47:03 +0000

    Deploying to gh-pages from @ Meyioo/svelte-todo-app@d9c9020ac1b506bdffe29273d7e33d5971f58e7e 🚀

--- _app/immutable/entry/start.dQ0NDMw_.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CZi1dq2w.js";export{t as start};
---
+++ _app/immutable/nodes/5.BMf-iZC5.js
@@ -0,0 +1,1 @@
+import"../chunks/disclose-version.CTHOEoPE.js";import{T as t}from"../chunks/todoList.YfpkpSnv.js";function c(o){t(o,{completed:!0})}export{c as component};
